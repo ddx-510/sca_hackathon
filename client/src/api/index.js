563a05caf771e05bddbc5fb389199313965a479e
ddx-510 <dai.tianle@dhs.sg>
2022-07-24 04:07:09 +0800

    Add dashboard view

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,3 +1,5 @@
+import { streamUrl } from "../utilities/url";
+
 export async function login({ email, password }) {
   return await fetch("/api/auth/login", {
     method: "POST",
@@ -33,4 +35,20 @@ export async function searchArtworks({ keyword }) {
     .catch((err) => {
       console.log(err);
     });
+}
+
+export async function listenFromStream(){
+  const sse = new EventSource(streamUrl, { withCredentials: true });
+  function getRealtimeData(data) {
+    // process the data here,
+    // then pass it to state to be rendered
+  }
+  sse.onmessage = e => getRealtimeData(JSON.parse(e.data));
+  sse.onerror = () => {
+    // error log here 
+    sse.close();
+  }
+  return () => {
+    sse.close();
+  };
 }
---
+++ client/src/components/homepage/dashboard.js
@@ -0,0 +1,35 @@
+import { Button, Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export let dashboard = (onLogout) => {
+    return <div id="viewport">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css"/>
+        <div id="sidebar">
+            <header>
+                <a href="#">My App</a>
+            </header>
+            <Nav defaultActiveKey="/home" className="flex-column">
+            <Nav.Link href="/home"><i class="zmdi zmdi-view-dashboard"></i> Dashboard</Nav.Link>
+            <Nav.Link eventKey="link-1"><i class="zmdi zmdi-info-outline"></i> About</Nav.Link>
+            <Nav.Link eventKey="link-2"> <i class="zmdi zmdi-link"></i> Shortcuts</Nav.Link>
+            <Nav.Link eventKey="disabled" disabled><i class="zmdi zmdi-calendar"></i> Events</Nav.Link>
+            </Nav>
+        </div>
+        <Navbar bg='light'>
+            <Container>
+                <Navbar.Brand></Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text>
+                    <Button variant="outline-danger" onClick={onLogout}>
+                    Log out
+                    </Button>
+                </Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        <Container fluid>
+        </Container>
+    </div>
+}
